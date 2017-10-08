4c3a147363db6283aef3ff14f55258f77bb45a1e
alexkch <alexc0219@gmail.com>
2017-10-08 19:54:08 -0400

    changed user relation to endorsement to has_many

--- prj/app/models/user.rb
+++ prj/app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_one :profile
   has_one :payment
   has_many :address
-  has_one :r_endorsement, :class_name => 'Endorsement', :foreign_key => 'ref_user_id'
-  has_one :g_endorsement, :class_name => 'Endorsement', :foreign_key => 'end_user_id'	
+  has_many :r_endorsement, :class_name => 'Endorsement', :foreign_key => 'ref_user_id'
+  has_many :g_endorsement, :class_name => 'Endorsement', :foreign_key => 'end_user_id'	
   validates :username, presence: true,
                     length: { minimum: 6 }
   validates :password, presence: true,
