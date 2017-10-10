60d896b1fa15966e3c8e2d27e08fec4405946a01
Maithri Basavapatna <32489428+maithrisb@users.noreply.github.com>
2017-10-10 12:37:48 -0400

    Updated bank_infos model

--- prj/db/migrate/20171008203207_create_bank_infos.rb
+++ prj/db/migrate/20171008203207_create_bank_infos.rb
@@ -2,12 +2,10 @@ class CreateBankInfos < ActiveRecord::Migration[5.1]
   def change
     create_table :bank_infos do |t|
       t.references :payment, foreign_key: true
-      t.string :bank_name
-      t.string :account_type
       t.string :account_name
       t.integer :account_number
       t.integer :transit_number
-
+      t.integer :institution_id
       t.timestamps
     end
   end
