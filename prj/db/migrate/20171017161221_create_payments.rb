3ef6f2da0593023a22b9f20eb766fd424264844a
maithribasavapatna <maithri.basavapatna@mail.utoronto.ca>
2017-10-17 12:13:38 -0400

    updates payments migration file

--- prj/db/migrate/20171017161221_create_payments.rb
+++ prj/db/migrate/20171017161221_create_payments.rb
@@ -1,10 +1,8 @@
 class CreatePayments < ActiveRecord::Migration[5.1]
   def change
     create_table :payments do |t|
-      t.references :user, foreign_key: true
       t.integer :paymenttype
-
-      t.timestamps
+      t.references :user, foreign_key: true
     end
   end
 end
