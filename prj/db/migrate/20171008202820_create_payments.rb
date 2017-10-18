2644751003db503aac93680bf8a09d2070d9b0bc
maithribasavapatna <maithri.basavapatna@mail.utoronto.ca>
2017-10-18 00:02:42 -0400

    reverted original payments migration file and added another addcolumn migration file

--- prj/db/migrate/20171008202820_create_payments.rb
+++ prj/db/migrate/20171008202820_create_payments.rb
@@ -1,8 +1,9 @@
 class CreatePayments < ActiveRecord::Migration[5.1]
   def change
     create_table :payments do |t|
-      t.integer :paymenttype
       t.references :user, foreign_key: true
+
+      t.timestamps
     end
   end
 end
