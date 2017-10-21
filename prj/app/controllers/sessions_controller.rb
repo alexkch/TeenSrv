8d7fc94fd0c7642a61bad34c331d98165f60425a
tenzint <tenzin.thabkhae@mail.utoronto.ca>
2017-10-21 18:25:30 -0400

    Added Sessions_helper, sessions_test, and updated(created?) sessions_controller.

--- prj/app/controllers/sessions_controller.rb
+++ prj/app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
+  	render 'new'
+  end
+
+  def create
+  	user = User.find_by(username: params[:session][:username].downcase)
+  	if(user && user.authenticate(params[:session][:password]))
+  		log_in user
+  		redirect_to user_path
+
+  	else
+  		flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+  		render 'new'
+  	end
+  end
+  def destroy
+
   end
 end
