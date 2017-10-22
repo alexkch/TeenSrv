691cdb097d29823236087c28a94500ad77bf7a09
tenzint <tenzin.thabkhae@mail.utoronto.ca>
2017-10-22 14:14:59 -0400

    Created a basic view to check login errors

--- prj/app/controllers/sessions_controller.rb
+++ prj/app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(username: params[:session][:username].downcase)
+  	user = User.find_by(username: params[:session][:username])
   	if(user && user.authenticate(params[:session][:password]))
-  		log_in user
-  		redirect_to user_path
+  		log_in userhjhj
+  		redirect_to user_path(user.id)
 
   	else
   		flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
