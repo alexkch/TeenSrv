35013f538b05fd82e18f7be24592df74ea5dacda
alexkch <alexc0219@gmail.com>
2017-10-24 12:55:25 -0400

    changed user model to include devise

--- prj/app/models/user.rb
+++ prj/app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-  # enum usertype: [:teen, :client, :cust_srv, :admin, :null]
-  # after_initialize :set_default_role, :if => :new_record?
-
-  # def set_default_role
-  #   self.usertype ||= :null
-  # end
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_one :teenager, dependent: :destroy
   has_one :client, dependent: :destroy
@@ -12,12 +9,5 @@ class User < ApplicationRecord
   has_many :address
   has_many :r_endorsement, :class_name => 'Endorsement', :foreign_key => 'ref_user_id'
   has_many :g_endorsement, :class_name => 'Endorsement', :foreign_key => 'end_user_id'	
-  validates :username, presence: true,
-                    length: { minimum: 6 }
-  validates :username, format: {
-                      with: /\A[\w+\-]+@[a-z\d\-.]+\.[a-z]+\z/i,
-                      message: "malformed email address"
-                    }
-  validates :password, presence: true,
-                    length: { minimum: 1 }
+
 end
