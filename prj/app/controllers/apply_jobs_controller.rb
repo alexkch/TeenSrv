feb63e849122a3374583ec1a01daa356abbda73d
tzaitoun <talat.zaitoun@mail.utoronto.ca>
2017-11-17 19:07:33 -0500

    Added fixes for applied_jobs

--- prj/app/controllers/apply_jobs_controller.rb
+++ prj/app/controllers/apply_jobs_controller.rb
@@ -1,13 +1,13 @@
 class ApplyJobsController < ApplicationController
 
 	def create 
-		@current_user = User.find(current_user.id)
+		@current_user = current_user
 		@job = Job.find(params[:apply_job])
 		
 		@apply_job = ApplyJob.new
 		@apply_job.job_id = @job.id
 		@apply_job.client_id = @job.client_id
-		@apply_job.teenager_id = @current_user.teenager
+		@apply_job.teenager_id = @current_user.teenager.id
 
 		@apply_job.save
 
@@ -16,7 +16,12 @@ class ApplyJobsController < ApplicationController
 
 	def index
 		@current_user = current_user
-		@apply_jobs = ApplyJob.where("teenager_id = ?", @current_user.id)
+		@teenager_id = @current_user.teenager.id
+
+		@rewarded_jobs = ApplyJob.where(teenager_id: @teenager_id, filled: true, winner_id: @teenager_id) 
+		@open_jobs = ApplyJob.where(teenager_id: @teenager_id, job_id: Job.where(accepted: false, cancelled: false))
+		#@filled_jobs = ApplyJob.where("teenager_id = ? AND filled = ? AND winner_id != ?", @teenager_id, true, @teenager_id)
+		#@cancelled_jobs = ApplyJob.where(teenager_id: @teenager_id, job_id: Job.where(cancelled: true))
 	end
 
 	private
