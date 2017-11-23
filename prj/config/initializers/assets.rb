471b0cf740f553109e859c94c57e758051ffa99e
alexkch <alexc0219@gmail.com>
2017-11-23 14:31:47 -0500

    added new initializer to precompile file

--- prj/config/initializers/assets.rb
+++ prj/config/initializers/assets.rb
@@ -8,14 +8,14 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-%w( scroll bootstrap.min jquery sidebar profilemenu stripe_connect ).each do |filename|
+%w( scroll bootstrap.min jquery sidebar profilemenu stripe_connect myjobs).each do |filename|
   Rails.application.config.assets.precompile += ["#{filename}.js"]
 end
 
 %w( welcome users home profile ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.css"]
 end
-%w( index global sidebar style1 ).each do |filename|
+%w( index global sidebar style1 search myjobs ).each do |filename|
   Rails.application.config.assets.precompile += ["#{filename}.css"]
 end
 # Precompile additional assets.
