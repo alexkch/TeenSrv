3b8ea1981546f2f0cd57f0512f4dd11298249389
alexkch <alexc0219@gmail.com>
2017-11-24 21:46:06 -0500

    added new assets

--- prj/config/initializers/assets.rb
+++ prj/config/initializers/assets.rb
@@ -8,14 +8,14 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-%w( scroll bootstrap.min jquery sidebar profilemenu stripe_connect myjobs).each do |filename|
+%w( scroll bootstrap.min jquery sidebar profilemenu stripe_connect myjobs modal_endorse ).each do |filename|
   Rails.application.config.assets.precompile += ["#{filename}.js"]
 end
 
 %w( welcome users home profile ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.css"]
 end
-%w( index global sidebar forms search myjobs ).each do |filename|
+%w( index global sidebar forms search myjobs modal_endorse ).each do |filename|
   Rails.application.config.assets.precompile += ["#{filename}.css"]
 end
 # Precompile additional assets.
