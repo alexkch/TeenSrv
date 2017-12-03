cc1fee5bb67ac0fcf929edf1019e0688007e0296
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2017-12-02 20:27:27 -0500

    Score system for teen applicants - works. New job start & end time is now date time selector

--- prj/app/controllers/accept_jobs_controller.rb
+++ prj/app/controllers/accept_jobs_controller.rb
@@ -8,31 +8,12 @@ class AcceptJobsController < ApplicationController
 			@client = Client.find_by_user_id(@user.id)
 			# @applications = ApplyJob.where("client_id = ? AND filled = false", @client.id).order(:job_id)
 			
-			@applications = ApplyJob.includes(:job, :teenager).where("client_id = ? AND filled = ?", @client.id, false)
-			@applications.each do | app |
-				endCount = Endorsement.where(end_user_id: app.teenager_id).count
-
-				allRatings = FinishedJob.where(teenager_id: app.teenager_id)
-				rCount = allRatings.count
-
-				total = endCount + rCount
-
-				rScore = 0
-
-				allRatings.each do |rating|
-					rScore = rScore + rating.teens_rating
-				end
-
-				RatingScore = RateScore / rcount
-
-				FinalScore = endCount * log(total/endcount) * 3.5 + RatingScore
-
-			end
-
-			@jobs = Job.all
+			@applications = ApplyJob.where("client_id = ?", @client.id)
+			@applications = @applications.eager_load(:job, :teenager).where("cancelled = ? AND finished = ? AND accepted = ?", false, false, false)
+			
+			@jobs = Job.where("client_id = ? AND cancelled = ? AND accepted = ? AND finished = ?", @client.id, false, false, false)
 		end
 	end
-
 
 	def new
 		@user = current_user
@@ -41,7 +22,34 @@ class AcceptJobsController < ApplicationController
 		end
 		@client = Client.find_by_user_id(@user.id)
 		@job = Job.find(params[:job])
-		@applications = ApplyJob.includes(:job, :teenager).where("client_id = ? AND job_id = ? AND filled = ?", @client.id, @job.id, false)
+		@applications = ApplyJob.eager_load(:job, :teenager).where("apply_jobs.client_id = ? AND job_id = ? AND filled = ? AND jobs.cancelled = ? AND jobs.finished = ? AND jobs.accepted = ?", @client.id, @job.id, false, false, false, false)
+		@appliers = {}
+		@applications.each do | app |
+			endCount = Endorsement.where(end_user_id: app.teenager_id).count
+			allRatings = FinishedJob.where(teenager_id: app.teenager_id)
+			rCount = allRatings.count
+
+			total = endCount + rCount
+
+			rScore = 0
+
+			allRatings.each do |rating|
+				rScore = rScore + rating.teens_rating
+			end
+
+			if(total == 0)
+				finalScore = 0
+			elsif(endCount == 0)
+				finalScore = rScore * Math.log(1.2)
+			elsif(rCount == 0)
+				finalScore = endCount * Math.log(1.2)
+			else
+				finalScore = endCount * Math.log(total * 1.2 /endcount) + rScore * Math.log(total *1.2 /rCount)
+			end
+			@appliers[app.teenager_id] = finalScore
+		end
+		@appliers = @appliers.sort_by { |k, v| v }.reverse		# sorts all appliers based on their finalScore in descending ordeer
+		# access first teenager_id by: @appliers[0][0], second: @appliers[1][0]; etc
 	end
 	def create
 		@user = current_user
